c75a2ae013b4ab6d612eb4b42394a827698e0cec
sajid_ansari <husantarabegam4556@gmail.com>
2025-01-08 18:02:26 +0530

    all done

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path=require('path')
 const express=require("express")
 const dotenv=require("dotenv")
 const cookieParser = require('cookie-parser');
@@ -14,6 +15,8 @@ dotenv.config()
 
 const PORT=process.env.PORT || 5000;
 
+// const __dirname = path.resolve();
+
 
 app.use(express.json())
 app.use(cookieParser())
@@ -21,6 +24,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api/auth",authroutes)
 app.use("/api/messages",messageRoutes)
 app.use("/api/user",userRoutes)
+
+
+app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+
+})
 
 
 server.listen(PORT,()=>{

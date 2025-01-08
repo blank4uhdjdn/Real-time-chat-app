d3e89915adccfe440065360db8144876802b810a
sajid_ansari <husantarabegam4556@gmail.com>
2025-01-08 20:38:32 +0530

    socket.io

--- frontend/src/context/socketContext.jsx
+++ frontend/src/context/socketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider=({children})=>{
     const{authUser}=useAuthContext()
     useEffect(()=>{
         if(authUser){
-            const socket=io('http://localhost:8000',{
+            const socket=io('https://real-time-chat-app-1-8vi4.onrender.com/',{
                     query:{
                     userId:authUser._id
                 }
